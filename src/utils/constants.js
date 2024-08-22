4d7b639a302ae1a1b006f6dc8783611471a676c8
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-08-22 18:38:25 +0530

    Added useState Hook

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/";
+export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8GVif7UfMcNVKpufdqt6GwX0j7hpxexSWmg&s";
